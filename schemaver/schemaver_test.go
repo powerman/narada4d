39c0647afef1ccfac847614ca3b6e53d15621487
salamandra19 <tatyana.krivonos@gmail.com>
2018-03-27 12:21:49 +0300

    test-plan raw

---
+++ schemaver/schemaver_test.go
@@ -0,0 +1,11 @@
+package schemaver
+
+import "testing"
+
+// - EnvLocation = ""
+// - EnvLocation = " "
+// - EnvLocation = "NARADA4D" success
+// - registered[loc.Scheme] = nil
+func TestLocation(t *testing.T) {
+
+}
