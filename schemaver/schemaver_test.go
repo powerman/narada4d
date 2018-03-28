0136b10f907cccbc3deb88b52617f14d62086ff2
salamandra19 <tatyana.krivonos@gmail.com>
2018-03-28 13:23:18 +0300

    test_plan

--- schemaver/schemaver_test.go
+++ schemaver/schemaver_test.go
@@ -2,43 +2,56 @@ package schemaver
 
 import "testing"
 
-// - EnvLocation = "", error
-// - EnvLocation = " ", error
-// - EnvLocation = "NARADA4D" success
+// - file://user@/
+// - file://localhost/
+// - file://?a=1
+// - file://#a
+// - file://path/to/empty/dir/, success
 // - registered[loc.Scheme] = nil, error "unknown protocol .."
 func TestLocation(t *testing.T) {
 
 }
 
 // - already Initialized, error
+// - file:///path/to/empty/dir/, success
 func TestInitialize(t *testing.T) {
 
 }
 
 // - version is not initialized, error
-// - EnvSkipLock != "", locktype = exclusive
+// - after initialize, success
 func TestNew(t *testing.T) {
 
 }
 
-// - EX1, SH1, SH1, UN1
-// - SH1, SH2, UN1, UN2
+// - lockType != unlocked, !SharedLock
+// - recursive lock, unlock one time
+// - Get(version)
+// - callback(version)
+// - SH, EX - panic
 func TestSharedLock(t *testing.T) {
 
 }
 
-// - EX1, EX1, SH1, UN1
-// - SH1, EX1, panic
+//- lockType = exclusive, !ExclusiveLock
+// - recursive lock, unlock one time
+// - Get(version)
+// - callback(version)
+// - "NARADA_SKIP_LOCK", !SharedLock, !ExclusiveLock, "NARADA_SKIP_LOCK" = ""
 func TestExclusiveLock(t *testing.T) {
 
 }
 
 // - Get() (lockType==unlocked), panic
+// - Get() (lockType==shared), success
+// - Get() (lockType==exclusive), success
 func TestGet(t *testing.T) {
 
 }
 
 // - Set() (lockType!= exclusive), panic
+// - Set() (lockType==shared), success
+// - Set() (lockType==exclusive), success
 func TestSet(t *testing.T) {
 
 }
