2b0d356246ba71986c5da77be4b968acdc16122e
Alex Efros <powerman@powerman.name>
2018-03-31 16:53:41 +0300

    add mock for schemaver tests

--- schemaver/schemaver_test.go
+++ schemaver/schemaver_test.go
@@ -1,14 +1,36 @@
 package schemaver_test
 
 import (
+	"errors"
+	"net/url"
+	"os"
 	"testing"
+
+	"github.com/powerman/check"
+	"github.com/powerman/narada4d/schemaver"
 )
+
+func init() {
+	schemaver.RegisterProtocol("test", schemaver.Backend{
+		New:        mockNew,
+		Initialize: mockInitialize,
+	})
+}
+
+// TODO
+func TestRegisterProtocol(tt *testing.T) {
+
+}
 
 // - test://localhost/, error
 // - test://, success
 // - registered[loc.Scheme] = nil, error "unknown protocol .."
 func TestLocation(tt *testing.T) {
+	t := check.T(tt)
+	os.Setenv(schemaver.EnvLocation, "test://localhost/")
+	t.Err(schemaver.Initialize(), errBadLocation)
 
+	// TODO
 }
 
 // - test:///ready, error
@@ -25,8 +47,37 @@ func TestNew(t *testing.T) {
 
 // - SH (with backend, return version), UN (with backend)
 // - NARADA_SKIP_LOCK=1, SH (no backend, return version), UN (no backend)
-func TestSharedLock(t *testing.T) {
+func TestSharedLock(tt *testing.T) {
+	t := check.T(tt)
+	reset()
 
+	cases := []struct {
+		setEnv      bool
+		envValue    string
+		wantBackend bool
+	}{
+		{false, "", true},
+		{true, "", true},
+		{true, "1", false},
+		{true, "anything", false},
+	}
+	for _, c := range cases {
+		if c.setEnv {
+			os.Setenv(schemaver.EnvSkipLock, c.envValue)
+		} else {
+			os.Unsetenv(schemaver.EnvSkipLock)
+		}
+		v, err := schemaver.New()
+		t.Nil(err)
+
+		old := sh
+		t.Equal(v.SharedLock(), "42")
+		if c.wantBackend {
+			t.Equal(sh, old+1, "set=%v val=%q", c.setEnv, c.envValue)
+		} else {
+			t.Equal(sh, old, "set=%v val=%q", c.setEnv, c.envValue)
+		}
+	}
 }
 
 // - EX (with backend, return version), UN (with backend)
@@ -74,3 +125,45 @@ func TestRecursiveLocks(t *testing.T) {
 func TestCallback(t *testing.T) {
 
 }
+
+var (
+	errBadLocation    = errors.New("location must not contain host")
+	errInitialized    = errors.New("version already initialized")
+	errNotInitialized = errors.New("version is not initialized")
+	sh, ex, un        int
+	ver               string
+)
+
+func reset() {
+	os.Unsetenv(schemaver.EnvSkipLock)
+	os.Setenv(schemaver.EnvLocation, "test://")
+	ver, sh, ex, un = "42", 0, 0, 0
+}
+
+func mockInitialize(loc *url.URL) error {
+	if loc.Host != "" {
+		return errBadLocation
+	}
+	if loc.Path == "/ready" {
+		return errInitialized
+	}
+	return nil
+}
+
+func mockNew(loc *url.URL) (schemaver.Manage, error) {
+	if loc.Host != "" {
+		return nil, errBadLocation
+	}
+	if loc.Path == "/empty" {
+		return nil, errNotInitialized
+	}
+	return &mockManage{}, nil
+}
+
+type mockManage struct{}
+
+func (m *mockManage) SharedLock()    { sh++ }
+func (m *mockManage) ExclusiveLock() { ex++ }
+func (m *mockManage) Unlock()        { un++ }
+func (m *mockManage) Get() string    { return ver }
+func (m *mockManage) Set(v string)   { ver = v }
