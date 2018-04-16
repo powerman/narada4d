f105086b544403e586c8d4a2315bd1b151d77757
salamandra19 <tatyana.krivonos@gmail.com>
2018-04-16 08:50:20 +0300

    funcs connect and parse added

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -2,12 +2,29 @@ package mysql
 
 import (
 	"database/sql"
-	"log"
+	"errors"
 	"net/url"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/powerman/narada4d/schemaver"
+)
+
+const (
+	createTable = `
+			CREATE TABLE Narada4D(
+			var VARCHAR(255) PRIMARY KEY,
+			val VARCHAR(255) NOT NULL
+			)
+			SELECT "version" as var, "none" as val
+			`
+
+	isInitialized = `SELECT COUNT(*) FROM Narada4D`
+	sharedLock    = `LOCK TABLE Narada4D READ`
+	exclusiveLock = `LOCK TABLE Narada4D WRITE`
+	unlock        = `UNLOCK TABLES`
+	getVersion    = `SELECT val FROM Narada4D WHERE var='version'`
+	setVersion    = `UPDATE Narada4D SET val=? WHERE var='version'`
 )
 
 type storage struct {
@@ -21,24 +38,12 @@ func init() {
 	})
 }
 
-func initialize(loc *url.URL) error {
+func connect(loc *url.URL) (*storage, error) {
 	s := &storage{}
-	var err error
-	s.db, err = sql.Open("mysql", loc.String())
+	err := parse(loc)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	_, err = s.db.Exec(`CREATE TABLE Narada4D(var VARCHAR (255) PRIMARY KEY, val VARCHAR(255) NOT NULL) SELECT "version" as var, "none" as val`)
-
-	if err != nil {
-		return (err)
-	}
-	s.db.Close()
-	return err
-}
-
-func new(loc *url.URL) (v schemaver.Manage, err error) {
-	s := &storage{}
 	pass, _ := loc.User.Password()
 
 	cfg := mysql.NewConfig()
@@ -56,57 +61,85 @@ func new(loc *url.URL) (v schemaver.Manage, err error) {
 	if err != nil {
 		return nil, err
 	}
+	return s, err
+}
+
+func parse(loc *url.URL) error {
+	if loc.User.Username == nil || loc.User.Password == nil || loc.Host == "" || loc.Path == "" || loc.RawQuery != "" || loc.Fragment != "" {
+		return errors.New("location muct contain only User, Host, Path")
+	}
+	return nil
+}
+
+func initialize(loc *url.URL) error {
+	s, err := connect(loc)
+	if err != nil {
+		return err
+	}
+	_, err = s.db.Exec(createTable)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := s.db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	return err
+}
+
+func new(loc *url.URL) (v schemaver.Manage, err error) {
+	s, err := connect(loc)
+	if err != nil {
+		panic(err)
+	}
 	return s, nil
 }
 
-func initialized() bool {
-	s := &storage{}
-	_, err := s.db.Query(`SELECT COUNT(*) FROM Narada4D`)
+func (s *storage) initialized() bool {
+	v, err := s.db.Query(isInitialized)
 	if err != nil {
-		return false
+		panic(err)
 	}
+	defer func() {
+		err := v.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
 	return true
 }
 
 func (s *storage) SharedLock() {
-	_, err := s.db.Exec(`LOCK TABLE Narada4D READ`)
+	_, err := s.db.Exec(sharedLock)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (s *storage) ExclusiveLock() {
-	_, err := s.db.Exec(`LOCK TABLE Narada4D WRITE`)
+	_, err := s.db.Exec(exclusiveLock)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (s *storage) Unlock() {
-	_, err := s.db.Exec(`UNLOCK TABLES`)
+	_, err := s.db.Exec(unlock)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (s *storage) Get() string {
-	v, err := s.db.Query(`SELECT val FROM Narada4D WHERE var='version'`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer v.Close()
 	var version string
-	for v.Next() {
-		if err := v.Scan(&version); err != nil {
-			log.Fatal(err)
-		}
-	}
-
+	_ = s.db.QueryRow(getVersion).Scan(&version)
 	return version
 }
 
 func (s *storage) Set(ver string) {
-	_, err := s.db.Exec(`UPDATE Narada4D SET val=? WHERE var='version'`, ver)
+	_, err := s.db.Exec(setVersion, ver)
 	if err != nil {
 		panic(err)
 	}
