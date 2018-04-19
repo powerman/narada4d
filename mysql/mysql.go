2ff424ac6a8c9caa249be2781e599cd40535e716
salamandra19 <tatyana.krivonos@gmail.com>
2018-04-19 12:26:31 +0300

    cleanned

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
@@ -11,7 +12,7 @@ import (
 )
 
 const (
-	createTable = `
+	sqlCreateTable = `
 			CREATE TABLE Narada4D(
 			var VARCHAR(255) PRIMARY KEY,
 			val VARCHAR(255) NOT NULL
@@ -19,12 +20,12 @@ const (
 			SELECT "version" as var, "none" as val
 			`
 
-	isInitialized = `SELECT COUNT(*) FROM Narada4D`
-	sharedLock    = `LOCK TABLE Narada4D READ`
-	exclusiveLock = `LOCK TABLE Narada4D WRITE`
-	unlock        = `UNLOCK TABLES`
-	getVersion    = `SELECT val FROM Narada4D WHERE var='version'`
-	setVersion    = `UPDATE Narada4D SET val=? WHERE var='version'`
+	sqlInitialized   = `SELECT COUNT(*) FROM Narada4D`
+	sqlSharedLock    = `LOCK TABLE Narada4D READ`
+	sqlExclusiveLock = `LOCK TABLE Narada4D WRITE`
+	sqlUnlock        = `UNLOCK TABLES`
+	sqlGetVersion    = `SELECT val FROM Narada4D WHERE var='version'`
+	sqlSetVersion    = `UPDATE Narada4D SET val=? WHERE var='version'`
 )
 
 type storage struct {
@@ -40,18 +41,17 @@ func init() {
 
 func connect(loc *url.URL) (*storage, error) {
 	s := &storage{}
-	err := parse(loc)
+	err := validate(loc)
 	if err != nil {
 		return nil, err
 	}
-	pass, _ := loc.User.Password()
 
 	cfg := mysql.NewConfig()
 	cfg.User = loc.User.Username()
-	cfg.Passwd = pass
+	cfg.Passwd, _ = loc.User.Password()
 	cfg.Net = "tcp"
 	cfg.Addr = loc.Host
-	cfg.DBName = loc.Path
+	cfg.DBName = strings.TrimPrefix(loc.Path, "/")
 	cfg.Collation = "utf8mb4_general_ci"
 	cfg.ReadTimeout = 5 * time.Second
 	cfg.WriteTimeout = 5 * time.Second
@@ -64,9 +64,15 @@ func connect(loc *url.URL) (*storage, error) {
 	return s, err
 }
 
-func parse(loc *url.URL) error {
-	if loc.User.Username == nil || loc.User.Password == nil || loc.Host == "" || loc.Path == "" || loc.RawQuery != "" || loc.Fragment != "" {
-		return errors.New("location muct contain only User, Host, Path")
+func validate(loc *url.URL) error {
+	if loc.User == nil || loc.User.Username() == "" {
+		return errors.New("incorrect Username, require mysql://username[:password]@host[:port]/database")
+	} else if loc.Host == "" {
+		return errors.New("incorrect Host, require mysql://username[:password]@host[:port]/database")
+	} else if loc.Path == "" || loc.Path == "/" {
+		return errors.New("require database, mysql://username[:password]@host[:port]/database")
+	} else if loc.RawQuery != "" || loc.Fragment != "" {
+		return errors.New("require only  mysql://username[:password]@host[:port]/database")
 	}
 	return nil
 }
@@ -76,57 +82,43 @@ func initialize(loc *url.URL) error {
 	if err != nil {
 		return err
 	}
-	_, err = s.db.Exec(createTable)
+	_, err = s.db.Exec(sqlCreateTable)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		err := s.db.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
+	s.db.Close()
 	return err
 }
 
 func new(loc *url.URL) (v schemaver.Manage, err error) {
-	s, err := connect(loc)
-	if err != nil {
-		panic(err)
-	}
-	return s, nil
+	return connect(loc)
 }
 
 func (s *storage) initialized() bool {
-	v, err := s.db.Query(isInitialized)
+	v, err := s.db.Query(sqlInitialized)
 	if err != nil {
-		panic(err)
+		return false
 	}
-	defer func() {
-		err := v.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
+	v.Close()
 	return true
 }
 
 func (s *storage) SharedLock() {
-	_, err := s.db.Exec(sharedLock)
+	_, err := s.db.Exec(sqlSharedLock)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (s *storage) ExclusiveLock() {
-	_, err := s.db.Exec(exclusiveLock)
+	_, err := s.db.Exec(sqlExclusiveLock)
 	if err != nil {
 		panic(err)
 	}
 }
 
 func (s *storage) Unlock() {
-	_, err := s.db.Exec(unlock)
+	_, err := s.db.Exec(sqlUnlock)
 	if err != nil {
 		panic(err)
 	}
@@ -134,12 +126,15 @@ func (s *storage) Unlock() {
 
 func (s *storage) Get() string {
 	var version string
-	_ = s.db.QueryRow(getVersion).Scan(&version)
+	err := s.db.QueryRow(sqlGetVersion).Scan(&version)
+	if err != nil {
+		panic(err)
+	}
 	return version
 }
 
 func (s *storage) Set(ver string) {
-	_, err := s.db.Exec(setVersion, ver)
+	_, err := s.db.Exec(sqlSetVersion, ver)
 	if err != nil {
 		panic(err)
 	}
