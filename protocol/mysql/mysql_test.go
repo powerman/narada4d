66ee6fa5debc35084dc6321f8819179f4eac6727
Alex Efros <powerman@powerman.name>
2018-04-22 17:30:51 +0300

    protocol/mysql: add docker for tests

--- protocol/mysql/mysql_test.go
+++ protocol/mysql/mysql_test.go
@@ -1,6 +1,64 @@
 package mysql
 
-import "testing"
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/powerman/check"
+)
+
+const (
+	dbName = "gotest"
+	dbHost = "127.0.0.1"
+	dbUser = "gotestuser"
+	dbPass = "gotestpass"
+)
+
+var dbPort string
+var locUser, locRoot *url.URL
+
+var dockerIDs []string
+
+func TestMain(m *testing.M) {
+	_, err := docker("info")
+	if err != nil {
+		fmt.Println("SKIP:", err)
+		os.Exit(0)
+	}
+
+	dbPort, err = runMySQL()
+	if err != nil {
+		fmt.Println(err)
+		dockerCleanup()
+		os.Exit(1)
+	}
+
+	locUser, err = url.Parse(fmt.Sprintf("mysql://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName))
+	if err != nil {
+		panic(err)
+	}
+	locRoot, err = url.Parse(fmt.Sprintf("mysql://root@%s:%s/%s", dbHost, dbPort, dbName))
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+	check.Report()
+	dockerCleanup()
+	os.Exit(code)
+}
 
 // - mysql://tanya:010203@localhost:3306/PROJECT (success)
 // - mysql://tanya:010203@localhost:3306/
@@ -19,8 +77,9 @@ func TestConnect(t *testing.T) {
 
 // - mysql://tanya@localhost:3306/PROJECT, TABLE created
 // - mysql://tanya@localhost:3306/PROJECT, Connection drop, TABLE not created, error
-func TestInitialize(t *testing.T) {
-
+func TestInitialize(tt *testing.T) {
+	t := check.T(tt)
+	t.Nil(initialize(locUser))
 }
 
 //- Protocol registered, 'SELECT COUNT (*) FROM Narada4D', true
@@ -60,4 +119,87 @@ func TestGet(t *testing.T) {
 // - Protocol not registered, sqlSetVersion, val=43, panic
 func TestSet(t *testing.T) {
 
+}
+
+func dockerCleanup() {
+	for _, id := range dockerIDs {
+		if _, err := docker("kill", id); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func runMySQL() (port string, err error) {
+	id, err := docker("run", "-d", "--rm", "-P",
+		"-e", "MYSQL_ALLOW_EMPTY_PASSWORD=yes",
+		"-e", "MYSQL_DATABASE="+dbName,
+		"-e", "MYSQL_USER="+dbUser,
+		"-e", "MYSQL_PASSWORD="+dbPass,
+		"mysql")
+	if err != nil {
+		return "", err
+	}
+	id = strings.TrimSpace(id)
+	dockerIDs = append(dockerIDs, id)
+
+	port, err = dockerPort(id, "3306/tcp")
+	if err != nil {
+		return "", err
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?timeout=1s&readTimeout=1s&writeTimeout=1s",
+		dbUser, dbPass, dbHost, port, dbName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stdout := os.Stdout
+	os.Stdout = nil
+	mysql.SetLogger(log.New(ioutil.Discard, "", 0))
+	defer func() {
+		os.Stdout = stdout
+		mysql.SetLogger(log.New(os.Stderr, "[mysql] ", log.Ldate|log.Ltime|log.Lshortfile))
+	}()
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	for {
+		if err = db.PingContext(ctx); err == nil {
+			return port, nil
+		}
+		time.Sleep(time.Second)
+	}
+	return "", errors.New("failed to connect to mysql")
+}
+
+func dockerPort(id, internalPort string) (string, error) {
+	out, err := docker("inspect", id)
+	if err != nil {
+		return "", err
+	}
+	var inspect []struct {
+		NetworkSettings struct {
+			Ports map[string][]struct {
+				HostPort string
+			}
+		}
+	}
+	err = json.Unmarshal([]byte(out), &inspect)
+	if err != nil {
+		return "", err
+	}
+	port := inspect[0].NetworkSettings.Ports[internalPort][0].HostPort
+	if port == "" {
+		return "", errors.New("failed to detect port")
+	}
+	return port, nil
+}
+
+func docker(args ...string) (string, error) {
+	out, err := exec.Command("docker", args...).Output()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		fmt.Println("docker", strings.Join(args, " "))
+		fmt.Println(string(exitErr.Stderr))
+	}
+	return string(out), err
 }
